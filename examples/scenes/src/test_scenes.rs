926b01b0497c5f666e0dc4c91c6670311c13e81b
eugenesvk <eugenesvk@users.noreply.github.com>
2025-02-22 19:02:28 +0700

    upd dbg print

--- examples/scenes/src/test_scenes.rs
+++ examples/scenes/src/test_scenes.rs
@@ -274,17 +274,18 @@ mod impls {
             // if rad0      <=       d_end
               // &&    seg_end >= d_beg  { // our segment overlaps with this dash
             // if is_last {
-            // println!(
-              // "{}{}{: >2} {: >2} abs {: >4.1}° → {: >4.1}° Δ{: >3.1}° off {: >3.1}°¦{: >3.1}°\
-              // │ rel {: >4.1}° → {: >4.1}° Δ{: >3.1}°\
-              // │ dash {: >4.1}° → {: >4.1}° Δ{: >4.1}°\
-              // │ draw {: >4.1}° → {: >4.1}° ⇒ {: >3.1}° "
-              // ,if draw_len>0.{"✓ "}else{"  "},if is_last {"🛑"}else{" "} ,seg_count, j
-              // ,rad0    .to_degrees(),rad1    .to_degrees(),(rad1-rad0).to_degrees(), dash_off_deg, seg_off
-              // ,seg_beg .to_degrees(),seg_end .to_degrees(),(seg_end - seg_beg).to_degrees()
-              // ,d_beg   .to_degrees(),d_end   .to_degrees(),dash_i.to_degrees()
-              // ,draw_beg.to_degrees(),draw_end.to_degrees(),draw_len.to_degrees()
-              // );
+            // println!( //👁👀👓  seg={dash_off_deg: >3.1} % {dash_iter_len_deg: >3.1}
+            //   "{}👀{}{i: >3}¦{steps_delta_i: >3}{seg_count: >2} {j: >2}\
+            //   │ {r2beg: >4.1}+{: >4.1}={: >4.1}° ↷ {: >4.1}° Δ{: >3.1}° off {: >3.1}°¦{: >3.1}° \
+            //   │ rel {: >4.1}° ↷ {: >4.1}° Δ{: >3.1}°\
+            //   │ dash {: >4.1}° → {: >4.1}° Δ{: >4.1}°\
+            //   │ 🖉 {: >4.1}° → {: >4.1}° ⇒ {: >3.1}° "
+            //   ,if draw_len>0.{"🖉"}else{" "}, if is_last {"🛑"}else{" "}
+            //   ,seg0.to_degrees()    ,rad0    .to_degrees(),rad1    .to_degrees(),(rad1-rad0).to_degrees(), dash_off_deg,seg_off.to_degrees()
+            //   ,seg_beg .to_degrees(),seg_end .to_degrees(),(seg_end - seg_beg).to_degrees()
+            //   ,d_beg   .to_degrees(),d_end   .to_degrees(),dash_i.to_degrees()
+            //   ,draw_beg.to_degrees(),draw_end.to_degrees(),draw_len.to_degrees()
+            //   );
             // }
           } else { //println!("   inactive ({: >4.1}°)",dash_i.to_degrees());
             let d_end = d_beg + dash_i;
