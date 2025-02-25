0456b72b50a37525669dee364f983fc3cf8e7d8a
eugenesvk <eugenesvk@users.noreply.github.com>
2025-02-25 16:17:33 +0700

    cmt

--- examples/scenes/src/test_scenes.rs
+++ examples/scenes/src/test_scenes.rs
@@ -175,7 +175,7 @@ mod impls {
       let w_step = w_delta_avg / steps_delta_f; //12/2/45 0.13 to reach average
       let w_per_step_i:f64	= w_delta_avg / f64::from(steps_delta_i); //to reach average
       let px1 = 1./dpi/r0; //length of 1px (scaled) in rad
-      let mut step_gap = if dbg>=1 {0.} else {px1}; //fix conflation artifacts outside of debug by overlapping segments (except the last one)
+      let mut step_gap = if dbg>=1 {0.} else {px1}; //fix conflation artifacts outside of debug by overlapping segments (except the last one). <1 sometimes works, but not reliably at different zoom factor
 
       let col_avg = col_beg.lerp(col_end,0.5,Default::default());
 
